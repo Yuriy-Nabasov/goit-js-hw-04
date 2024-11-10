212b9986242774cdeedd0b6ed24f44f225165c9d
Yuriy Nabasov <nabasoff@gmail.com>
2024-11-10 12:51:26 +0200

    preparation

--- js/task-2.js
+++ js/task-2.js
@@ -1,22 +1,38 @@
-//? Задача 2. Композиція масивів
-// Напиши функцію під назвою makeArray, яка приймає три параметри:
-// firstArray(масив), secondArray(масив) і maxLength(число).
-// Функція повинна створювати новий масив, який містить усі елементи з firstArray,
-// а потім усі елементи з secondArray.
-// Якщо кількість елементів у новому масиві перевищує maxLength,
-// функція повинна повернути копію масиву з довжиною maxLength елементів.
-// В іншому випадку функція повинна повернути весь новий масив.
+//? Задача 2. Розрахунок калорій
+// Напиши функцію calcAverageCalories(days),
+// яка повертає середньодобове значення кількості калорій,
+// які спортсмен споживав протягом тижня.
+// Функція очікує один параметр: days — масив об’єктів.
+// Кожен об’єкт описує день тижня та кількість калорій calories, спожитих спортсменом, у цей день.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+// У консоль будуть виведені результати її викликів.
 
 `use strict`; // Код у суворому режимі
 
-function makeArray(firstArray, secondArray, maxLength) {
-  const newArray = firstArray.concat(secondArray);
-  return newArray.slice(0, maxLength); //! або  return firstArray.concat(secondArray).slice(0, maxLength);
-}
+function calcAverageCalories(days) {}
 
-console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
-console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
-console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
-console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
--- js/task-3.js
+++ js/task-3.js
@@ -1,29 +1,26 @@
-//? Задача 3. Фільтрація масиву чисел
-// Напиши функцію filterArray(numbers, value),
-// яка приймає масив чисел(numbers) та значення(value) як параметри.
-// Функція повинна повертати новий масив лише тих чисел із масиву numbers,
-// які більші за значення value.
-// Усередині функції:
-// Створи порожній масив, у який будеш додавати підходящі числа.
-// Використай цикл для ітерації кожного елемента масиву numbers.
-// Використовуй умовний оператор if усередині циклу для перевірки кожного елемента
-// и додавання до свого масиву.
-// Поверни свій новий масив з підходящими числами як результат.
+//? Задача 3. Профіль гравця
+// Об’єкт profile описує профіль користувача на ігровій платформі.
+// У його властивостях зберігається ім’я профілю username та кількість активних годин playTime, проведених у грі.
+
+// const profile = {username: "Jacob", playTime: 300,};
+
+// Доповни об’єкт profile методами для роботи з його властивостями.
+
+// Метод changeUsername(newName) повинен приймати рядок (нове ім’я) в параметр newName
+// та змінювати значення властивості username на нове.Нічого не повертає.
+// Метод updatePlayTime(hours) повинен приймати число (кількість годин) у параметр hours
+// та збільшити на нього значення властивості playTime.Нічого не повертає.
+// Метод getInfo() має повертати рядок формату <Username> has <amount> active hours!,
+// де < Username > — це ім’я профілю, а < amount > — кількість ігрових годин.
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+// У консоль будуть виведені результати її роботи.
 
 `use strict`; // Код у суворому режимі
 
-function filterArray(numbers, value) {
-  let newArray = [];
-  for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] > value) {
-      newArray.push(numbers[i]);
-    }
-  }
-  return newArray;
-}
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
